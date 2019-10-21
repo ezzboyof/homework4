30ed56cff8862ca1317116617320a73ecbe4b0b9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 20:58:16 +0300

    Update Main.java

--- src/homework4/task_1/Main.java
+++ src/homework4/task_1/Main.java
@@ -2,16 +2,20 @@ package homework4.task_1;
 
 public class Main {
     public static void main(String[] args) {
+        // имена переменных с маленькой буквы
+        // Student_1 -> archi
         Person Student_1 = new Person("Archi", 16);
+        // vlad
         Person Student_2 = new Person("Vlad", 15);
+        // noName
         Person Student_3 = new Person();
 
         Student_1.show();
-        System.out.println(Student_1.getName());
+        System.out.println(Student_1.getName()); // метод show() выводит все эти данные, зачем вызывать их повторно?
         System.out.println(Student_1.getAge());
 
         Student_2.show();
-        System.out.println(Student_2.getName());
+        System.out.println(Student_2.getName()); // метод show() выводит все эти данные, зачем вызывать их повторно?
         System.out.println(Student_2.getAge());
     }
 }
